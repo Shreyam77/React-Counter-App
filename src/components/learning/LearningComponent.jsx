4f5d9e3991f694203068efb658b1a3dae27cf3e9
Shreyam Gupta <56187671+Shreyam77@users.noreply.github.com>
2023-01-23 21:26:05 +0530

    Initial commit

---
+++ src/components/learning/LearningComponent.jsx
@@ -0,0 +1,11 @@
+import FirstComponent from './FirstComponent'
+import SecondComponent from './SecondComponent'
+
+export default function LearningComponent() {
+    return (
+      <div className="LearningComponent">
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+      </div>
+    );
+  }
---
+++ src/components/learning/SecondComponent.jsx
@@ -0,0 +1,6 @@
+export default function SecondaComponent()
+{
+  return (
+    <div className="SecondComponent">Second Component</div>
+  )
+}
